592094d333dae1ec7aefb8bdc343c84feab700c8
Nugget98 <61585323+Nugget98@users.noreply.github.com>
2022-10-11 12:29:05 +0200

    fix dialog

---
+++ Assets/Dialog.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialog
+{
+
+    [TextArea(1,10)]
+    public string[] Sentences;
+    public string Name;
+
+
+}
