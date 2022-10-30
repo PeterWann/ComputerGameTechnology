632c3ab8a58073da3129936e44cd9f794455e906
PeterWann <201907121@post.au.dk>
2022-10-30 10:54:42 +0100

    Enemy Hit working

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Animator anim;
+    [SerializeField]
+    private BoxCollider enemyCollider;
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private Rigidbody rb;
+
+    [SerializeField]
+    private int hitpoints = 40;
+    [SerializeField]
+    private float distanceToRun = 22f;
+    [SerializeField]
+    private float distanceToHit = 3.3f;
+    private float calcDistance;
+
+    private void Update()
     {
-        
+        calcDistance = Vector3.Distance(this.transform.position, player.transform.position);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.collider.name == "Limb(Clone)")
+        {
+            EnemyHit();
+            // Only first hit, ignore rest.
+            Physics.IgnoreCollision(collision.collider, enemyCollider);
+        }
     }
+
+    void EnemyHit()
+    {
+        hitpoints -= 20;
+        anim.SetTrigger("IsHit");
+
+        if (hitpoints <= 0)
+        {
+            EnemyDeath();
+        }
+    }
+
+    void EnemyDeath()
+    {
+        Destroy(rb);
+        Destroy(enemyCollider);
+        anim.SetBool("IsDead", true);
+    }
+
 }
